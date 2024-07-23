2f14298f41eaff0e1b6a87bfdf2e7ca1c1f15c15
Carinn Gaspar <gasca681@student.otago.ac.nz>
2024-07-23 15:17:45 +1200

    Changes: - runGame.bat (Run the game and compile on windows)

--- gameEngine.cpp
+++ gameEngine.cpp
@@ -2,47 +2,15 @@
 #include <string>
 #include <thread>
 #include <cstdio>
-#define UNICODE
+#include <windows.h>
 #include <chrono> // Include the chrono header
+
+#define UNICODE
 
 using namespace std;
 using namespace std::chrono; // To avoid typing std::chrono:: every time
 
-class Screen {
-public:
-    // Refresh the screen to a new frame.
-    void refreshScreen() {
-        // 12.33 milliseconds
-        //std::string command = "mode con: cols=" + std::to_string(screenWidth) + " lines=" + std::to_string(screenHeight);
-        //system(command.c_str());
-        system("cls");
-        // Print text here
-        //cout << txt1;
-        //cout.flush();
-        std::this_thread::sleep_for(chrono::milliseconds(10));
-    }
 
-    void changeScreenSize (int width, int height) {
-        screenWidth = width;
-        screenHeight = height;
-
-        std::string command = "mode con: cols=" + std::to_string(screenWidth) + " lines=" + std::to_string(screenHeight);
-        system(command.c_str());
-    }
-
-    void generateLine () {
-        cout << "█";
-        cout.flush();
-    }
-
-private:
-    string fullBox = "█";
-    int screenWidth = 120;
-    int screenHeight = 40;
-    string txt1 = "Dragons have always been a central figure in mythology, legends, and fantasy literature. Among the various species of dr\n";
-    string txt3 = "entities that embody the essence of the heavens. They are considered guardians of celestial bodies and cosmic balance. I";
-    string txt2 = "and hybrids such as the Solarus dragon stand out for their unique characteristics and lore.Celestial dragons are often d";
-};
 
 class Timer {
 public:
@@ -73,14 +41,14 @@ class Character {
     string character;
     string color = "White";
     string background = "White";
-    
-    Character (char cha, string col, string back) {
+
+    Character (string cha, string col, string back) {
         character = cha;
         color = col;
         background = back;
     }
 
-    Character (char cha, string col) {
+    Character (string cha, string col) {
         character = cha;
         color = col;
         background = col;
@@ -92,6 +60,71 @@ class Character {
     }
 };
 
+class Screen {
+public:
+    // Refresh the screen to a new frame.
+    void refreshScreen() {
+        // 12.33 milliseconds
+        //std::string command = "mode con: cols=" + std::to_string(screenWidth) + " lines=" + std::to_string(screenHeight);
+        //system(command.c_str());
+        system("cls");
+        // Print text here
+        //cout << txt1;
+        //cout.flush();
+        //std::this_thread::sleep_for(chrono::milliseconds(10));
+    }
+
+    void changeScreenSize (int width, int height) {
+        screenWidth = width;
+        screenHeight = height;
+
+        std::string command = "mode con: cols=" + std::to_string(screenWidth) + " lines=" + std::to_string(screenHeight);
+        system(command.c_str());
+
+    }
+
+    void generateLine () {
+        cout << "█";
+        cout.flush();
+    }
+
+    void loadScreen () {
+        
+        // Load current screen testing
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        string s = "";
+        for (int y = 0; y <= screenHeight; y++) {
+            for (int x = 0; x <= screenWidth; x++) {
+
+                
+
+                // Print the character
+                s = s + "1";
+
+                // Reset the text color to default
+                //SetConsoleTextAttribute(hConsole, 7);
+            }
+            
+        }
+
+        // Set the text color
+        int textColor = 1;
+        int bgColor = 1;
+        SetConsoleTextAttribute(hConsole, (bgColor << 4) | textColor);
+        cout << s;
+        SetConsoleTextAttribute(hConsole, 7);
+        
+    }
+
+private:
+    string fullBox = "█";
+    int screenWidth = 120;
+    int screenHeight = 200;
+    string txt1 = "Dragons have always been a central figure in mythology, legends, and fantasy literature. Among the various species of dr\n";
+    string txt3 = "entities that embody the essence of the heavens. They are considered guardians of celestial bodies and cosmic balance. I";
+    string txt2 = "and hybrids such as the Solarus dragon stand out for their unique characteristics and lore.Celestial dragons are often d";
+};
+
 class Game {
     public:
     void LoadGame () {
@@ -101,7 +134,18 @@ class Game {
         system("chcp 65001");
 
         // Set screen size to 120 x by 40 y
-        screen.changeScreenSize(120,40);
+        screen.changeScreenSize(150,50);
+
+        // Timer t;
+
+        // t.startTimer();
+
+        screen.loadScreen();
+        screen.refreshScreen();
+        screen.loadScreen();
+
+        // t.stopTimer();
+        // t.returnDuration();
     }
 };
 
@@ -111,8 +155,10 @@ int main() {
 
     game.LoadGame();
 
-    Character c = Character('e',"Red");
-    c.printChar();
+    system("@echo off");
+
+    //Character c = Character('e',"Red");
+    //c.printChar();
     
-    system("Pause");
+    //system("Pause");
 }
