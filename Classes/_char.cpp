b89fad6d2564bdf972f3b4977051de50f293698d
Sparkletinkercat <69016594+Sparkletinkercat@users.noreply.github.com>
2024-08-02 15:38:12 +1200

    Update _char.cpp
    FIXED M<EGA BUG

--- Classes/_char.cpp
+++ Classes/_char.cpp
@@ -1,10 +1,5 @@
-#include <iostream>
-#include <string>
-#include <thread>
-#include <cstdio>
-#include <windows.h>
-#include <fstream>
-#include <chrono> // Include the chrono header
+
+#include "_char.hpp" // Include the chrono header
 
 
 #define UNICODE
@@ -18,43 +13,29 @@ using namespace std::chrono;
 
 // =================================================
 
-class _char {
-    public:
-
-        void setColor (int color) {
+        void _char::setColor (int color) {
             txtColor = color;
             backgroundColor = color;
         }
 
-        void setColor (int txt, int background) {
+        void _char::setColor (int txt, int background) {
             txtColor = txt;
             backgroundColor = background;
         }
 
-        int getBackgroundColor () {return backgroundColor;}
-        int getTextColor () {return txtColor;}
+        int _char::getBackgroundColor () {return backgroundColor;}
+        int _char::getTextColor () {return txtColor;}
 
-        int getX () {return x;}
-        int getY () {return y;}
-        int getZ () {return z;}
+        int _char::getX () {return x;}
+        int _char::getY () {return y;}
+        int _char::getZ () {return z;}
 
-        void setXYZ (int X, int Y, int Z) {
+        void _char::setXYZ (int X, int Y, int Z) {
             x = X;
             y = Y;
             z = Z;
         }
 
-        void setChar (string c) {character = c;}
-        void setChar (boolean toNull) {isNull = toNull;}
-        string getChar () {return character;}
-
-    private:
-    int x = -1;
-    int y = -1;
-    int z = -1;
-
-    string character = "";
-    boolean isNull = false;
-    int txtColor = 1;
-    int backgroundColor = 15;
-};
+        void _char::setChar (string c) {character = c;}
+        void _char::setCharNull (bool toNull) {isNull = toNull;}
+        string _char::getChar () {return character;}
