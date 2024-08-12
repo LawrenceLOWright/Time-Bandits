43813105f16670f5e56ee28c9e09ac4727fc4a17
Sparkletinkercat <69016594+Sparkletinkercat@users.noreply.github.com>
2024-08-13 11:14:02 +1200

    Update fileToString.cpp

--- Classes/fileToString.cpp
+++ Classes/fileToString.cpp
@@ -16,10 +16,18 @@ using namespace std::chrono;
 
 class fileToString {
     public:
-        fileToString(string filePath, string colorPath) {
+        fileToString(string file) {
+            string filePath = file + ".txt";
+            string colorPath = file + ".color";
+            string bgColor = file + ".background";
+
             str = getText(filePath);
-            colors = getText(colorPath);
-            colors = colors.substr(2);
+
+            txtColor = getText(colorPath);
+            txtColor = txtColor.substr(2);
+
+            backgroundColor = getText(bgColor);
+            backgroundColor = backgroundColor.substr(2);
         }
 
         string getText (string filePath) {
@@ -51,22 +59,57 @@ class fileToString {
             // Convert each char into a _char array
             for (int x = 0; x < length; x++) {
                 string c(1, str.at(x));
-                string e(1, colors.at(x));
+                string e(1, txtColor.at(x));
+                string d(1, backgroundColor.at(x));
 
                 _char character;
                 if (c == nullChar) {character.setCharNull(true);} 
                 else {character.setChar(c);}
 
-                if (e == "b") {
-                    character.setColor(9);
-                }
+                int txt = 0;
+                int bg = 0;
+                
+                if (e == "0") { txt = 0; }
+                if (e == "1") { txt = 1; }
+                if (e == "2") { txt = 2; }
+                if (e == "3") { txt = 3; }
+                if (e == "4") { txt = 4; }
+                if (e == "5") { txt = 5; }
+                if (e == "6") { txt = 6; }
+                if (e == "7") { txt = 7; }
+                if (e == "8") { txt = 8; }
+                if (e == "b") { txt = 9; }
+                if (e == "g") { txt = 10; }
+                if (e == "c") { txt = 11; }
+                if (e == "r") { txt = 12; }
+                if (e == "m") { txt = 13; }
+                if (e == "y") { txt = 14; }
+                if (e == "w") { txt = 15; }
 
+                if (d == "0") { bg = 0; }
+                if (d == "1") { bg = 1; }
+                if (d == "2") { bg = 2; }
+                if (d == "3") { bg = 3; }
+                if (d == "4") { bg = 4; }
+                if (d == "5") { bg = 5; }
+                if (d == "6") { bg = 6; }
+                if (d == "7") { bg = 7; }
+                if (d == "8") { bg = 8; }
+                if (d == "b") { bg = 9; }
+                if (d == "g") { bg = 10; }
+                if (d == "c") { bg = 11; }
+                if (d == "r") { bg = 12; }
+                if (d == "m") { bg = 13; }
+                if (d == "y") { bg = 14; }
+                if (d == "w") { bg = 15; }
+
+
+                character.setColor(txt,bg);
 
                 vec.push_back(character);
+
                 // Print to screen (testing)
                 //cout << vec[x].getChar();
-
-                
             }
             return vec;
         }
@@ -75,7 +118,8 @@ class fileToString {
 
     private:
         string str;
-        string colors;
+        string txtColor;
+        string backgroundColor;
 
 };
 
