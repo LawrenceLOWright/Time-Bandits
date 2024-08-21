4334c345b12df00fcb8250dad4626fcbaad100e1
Jack Blair <blaja159@student.otago.ac.nz>
2024-08-21 12:13:36 +1200

    run game batch file working

--- runGame.bat
+++ runGame.bat
@@ -26,7 +26,7 @@ if '%errorlevel%' NEQ '0' (
 
 ::ENTER YOUR CODE BELOW:
 ::g++ -g Classes/*.cpp -o output_executable
-g++ -g gameEngine.cpp -o gameEngine.exe
+g++ -g -IClasses gameEngine.cpp Classes/_char.cpp -o gameEngine.exe
 gameEngine.exe
 pause
 :: NEW COMPILE METHOD
