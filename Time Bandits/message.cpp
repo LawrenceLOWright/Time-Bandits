4dd119f1eaf31b5e101e298c32db112159e9240c
Jack Blair <blaja159@student.otago.ac.nz>
2024-08-29 22:41:08 +1200

    should be enough comments for doxygen alpha, comments are tiering

--- Time Bandits/message.cpp
+++ Time Bandits/message.cpp
@@ -9,21 +9,32 @@
 #include <chrono> // Include the chrono header
 #include <vector>
 
-
 using namespace std;
 using namespace std::chrono;
 
 // =================================================
 // Sets the current background color and text color for any printed text.
-
-
 // =================================================
 
-
+/**
+ * @brief Sets the vector of messages for the object.
+ * 
+ * @param msg A vector of strings representing the messages to be stored.
+ */
 void message::setMessages(vector<string> msg) { messages = msg; }
 
+/**
+ * @brief Adds a single message to the existing list of messages.
+ * 
+ * @param msg A string representing the message to be added.
+ */
 void message::addMessage(string msg) { messages.push_back(msg); }
 
+/**
+ * @brief Prints all stored messages to the standard output.
+ * 
+ * Each message is printed on a new line.
+ */
 void message::printMessages() {
     for (int x = 0; x < messages.size(); x++) {
         cout << messages[x] << endl;
--- Time Bandits/message.h
+++ Time Bandits/message.h
@@ -9,25 +9,44 @@
 #include <chrono> // Include the chrono header
 #include <vector>
 
-
 using namespace std;
 using namespace std::chrono;
 
 // =================================================
 // Sets the current background color and text color for any printed text.
-
-
 // =================================================
 
+/**
+ * @class message
+ * @brief A class to handle and manipulate a collection of messages.
+ * 
+ * The message class provides functionalities to set, add, and print
+ * a collection of string messages. It acts as a simple container and
+ * manager for a list of messages.
+ */
 class message {
     public:
+        /**
+         * @brief Sets the vector of messages for the object.
+         * 
+         * @param msg A vector of strings representing the messages to be stored.
+         */
         void setMessages(vector<string> msg);
 
+        /**
+         * @brief Adds a single message to the existing list of messages.
+         * 
+         * @param msg A string representing the message to be added.
+         */
         void addMessage(string msg);
 
+        /**
+         * @brief Prints all stored messages to the standard output.
+         * 
+         * Each message is printed on a new line.
+         */
         void printMessages();
 
     private:
-        vector<string> messages;
+        vector<string> messages; /**< Vector to store messages */
 };
-
