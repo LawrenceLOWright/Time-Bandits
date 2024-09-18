0e1296eeea63c38e55bf8584c0c7e0fbdcb867e4
Carinn Gaspar <gasca681@student.otago.ac.nz>
2024-09-18 16:45:08 +1200

    Files I am working on rn to make things easier to debug

---
+++ Time Bandits/FileRetrival.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <cstdio>
+#include <windows.h>
+#include <fstream>
+#include <chrono> // Include the chrono header
+#include <vector>
+#include <map>
+#include "File.h"
+
+using namespace std;
+using namespace std::chrono;
